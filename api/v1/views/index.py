10b6119481c65928294ca88a73eee1d64a1b6841
Nikkolathi <mainginicholus@gmail.com>
2024-01-08 16:05:11 +0000

    index correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,30 @@
-#!/usr/bin/python3xx
-''' status of api '''
-import models
-from models import storage
-from models.base_model import BaseModel
-from flask import jsonify
+#!/usr/bin/python3
+"""index"""
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
-@app_views.route('/status', strict_slashes=False)
-def returnjson():
-    '''return json'''
-    return jsonify(status='OK')
+@app_views.route('/status', methods=['GET'])
+def status():
+    ''' routes to status page '''
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route("/stats", methods=["GET"], strict_slashes=False)
-def stats():
-    """Retrieves the number of objects per each type"""
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-    from models import storage
-    import json
-    dic = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    }
-    json_dict = json.dumps(dic, indent=2)
-    return json_dict
+@app_views.route('/stats', methods=['GET'])
+def count():
+    '''retrieves the number of each objects by type'''
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
